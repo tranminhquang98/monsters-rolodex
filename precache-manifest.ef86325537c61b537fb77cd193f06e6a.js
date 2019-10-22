b15a955eac50e74d4986ffa02459f30e1248a850
Quang Tran <tranminhquang0907@gmail.com>
2019-10-22 16:26:35 +0700

    Updates

---
+++ precache-manifest.ef86325537c61b537fb77cd193f06e6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a67620bc337519224fa6d15589e4cb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3e03119633d09d7e3c5",
+    "url": "/monsters-rolodex/static/css/main.c487bdaf.chunk.css"
+  },
+  {
+    "revision": "3663d9b86bcebd379dd1",
+    "url": "/monsters-rolodex/static/js/2.87b9e15c.chunk.js"
+  },
+  {
+    "revision": "f3e03119633d09d7e3c5",
+    "url": "/monsters-rolodex/static/js/main.cfc84668.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
